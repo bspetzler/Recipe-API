d1f707ec5e94b26122de6a0ae38114250f50ed9b
Brenden Spetzler <brendenspetzler@Brendens-iMac.local>
2020-04-21 18:20:00 -0600

    feedback corrections

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const STORE = {
   apiKey: '7675276694fb970af08da98b031a5175',
   appID: '0093db1a',
   searchURL: 'https://api.edamam.com/search',
+  youApiKey: 'AIzaSyA94uYzNGWPFgv_lpJqkJx_2oXup7KGb1w',
+  youSearchURL: 'https://www.googleapis.com/youtube/v3/search',
 }
-/*const apiKey = '7675276694fb970af08da98b031a5175';
-const appID = '0093db1a';
-const searchURL = 'https://api.edamam.com/search';
-*/
 
 function formatQueryParams(params) { //puts query together
   const queryItems = Object.keys(params)
@@ -44,47 +42,79 @@ function filterHealthLabel(arrayOfObjects, arrayOfFilters){ // uses the same met
   return newArray;
     };
 
-function displayResults(responseJson) {
+function displayResults(responseJson, responseTubes, dietLabels, healthLabels) {
   // if there are previous results, remove them
   $('#results-list').empty();
   $('#js-error-message').html("");
 
   // iterate through the items array
-  const dietLabels = Object.values($("input[class='dietLabels']:checked")).filter(function(e,i,arr){
+  /*const dietLabels = Object.values($("input[class='dietLabels']:checked")).filter(function(e,i,arr){
     return i < arr.length - 2; // there are 2 extra elements in this array always
   }).map(e => e.id);
   const healthLabels = Object.values($("input[class='healthLabels']:checked")).filter(function(e,i,arr){
     return i < arr.length - 2;
-  }).map(e => e.id);
+  }).map(e => e.id);*/
 
   if (dietLabels.length || healthLabels.length > 0){ // decide which use-case is needed. dietLabels and healthLabels are in 2 different arrays within the response object even though they look the same to the user
     if (dietLabels.length && healthLabels.length > 0){
       const result = filterHealthLabel(filterDietLabel(responseJson.hits, dietLabels), healthLabels);
-      createContent(result);
+      createContent(result, responseTubes);
     } else if (dietLabels.length > 0){
       const result = filterDietLabel(responseJson.hits, dietLabels);
-      createContent(result);
+      createContent(result, responseTubes);
     } else {
       const result = filterHealthLabel(responseJson.hits, healthLabels);
-      createContent(result);
+      createContent(result, responseTubes);
     }
   } else {
-    createContent(responseJson.hits);
+    createContent(responseJson.hits, responseTubes);
   }
 };
 
-function createContent(results){ // create content to display on page
-  if (results.length != 0){
-    for (let i = 0; i < results.length; i++){
+function createContent(results, responseJson){ // create content to display on page
+  if (responseJson.pageInfo.totalResults != 0 & results.length != 0){
+
+    // display videos
+    $('#results-list').append(
+      `<li><h3 class='search-results-header search-section header'>Videos</h3></li>`
+    )
+    
+    for (let i = 0; i < responseJson.items.length; i++){
       $('#results-list').append(
-        `<li class='result'>
-          <h3><a href='${results[i].recipe.url}' target='_blank'>${results[i].recipe.label}</a></h3>
-          <img class='result-img' style="width:266px" src='${results[i].recipe.image}' alt='${results[i].recipe.label} image'>
-          <ul class='result-ul'>
-            <li>${String(results[i].recipe.ingredientLines).replace(/,/g,'</li><li>').split('</li><li> ').join(', ').split('</li><li>for').join(' for').split('</li><li>chilled').join(' chilled').split('</li><li>well').join(' well')}</li>
-          </ul>
+        `<li class='result'><h3><a href='https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}' target='_blank'>${responseJson.items[i].snippet.title}</a></h3>
+        <img class='result-img' style="width:150px" src='${responseJson.items[i].snippet.thumbnails.default.url}' alt='${responseJson.items[i].snippet.title}'>
         </li>`
       )};
+
+      // display recipes
+      $('#results-list').append(
+        `<li><h3 class='search-results-header search-section header'>Recipes</h3></li>`
+      )
+
+      for (let i = 0; i < results.length; i++){
+        $('#results-list').append(
+          `<li class='result'>
+            <h3><a href='${results[i].recipe.url}' target='_blank'>${results[i].recipe.label}</a></h3>
+            <img class='result-img' style="width:266px" src='${results[i].recipe.image}' alt='${results[i].recipe.label} image'>
+            <ul class='result-ul'>
+              <li>${String(results[i].recipe.ingredientLines).replace(/,/g,'</li><li>').split('</li><li> ').join(', ').split('</li><li>for').join(' for').split('</li><li>chilled').join(' chilled').split('</li><li>well').join(' well')}</li>
+            </ul>
+          </li>`
+        )};
+    } else if (responseJson.pageInfo.totalResults != 0){
+      // display videos
+      $('#results-list').append(
+        `<li><h3 class='search-results-header search-section header'>Videos</h3></li>`
+      )
+      
+      for (let i = 0; i < responseJson.items.length; i++){
+        $('#results-list').append(
+          `<li class='result'><h3><a href='https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}' target='_blank'>${responseJson.items[i].snippet.title}</a></h3>
+          <img class='result-img' style="width:150px" src='${responseJson.items[i].snippet.thumbnails.default.url}' alt='${responseJson.items[i].snippet.title}'>
+          </li>`
+        )};
+      //display the results section  
+      $('#results').removeClass('hidden');
   } else {
     $('#results-list').append(
         `<h3>Sorry, no matches were found</h3>`
@@ -95,7 +125,7 @@ function createContent(results){ // create content to display on page
   $('#results').removeClass('hidden');
 }
 
-function getRests(ingredient) { //creates url and uses fetch to get data
+function getRests(ingredient, maxResults) { //creates url and uses fetch to get data
   const params = {
     app_key: STORE.apiKey,
     app_id: STORE.appID,
@@ -113,7 +143,49 @@ function getRests(ingredient) { //creates url and uses fetch to get data
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson))
+    .then(responseJson => getYouTubeVideos(ingredient, maxResults, responseJson))
+    .catch(err => {
+      $('#js-error-message').html("");
+      $('#js-error-message').append(`Oops, something must have gone wrong: wait and try again`);
+    })
+}
+
+function getYouTubeVideos(query, maxResults=10, responseJson) {
+  const dietLabels = Object.values($("input[class='dietLabels']:checked")).filter(function(e,i,arr){
+    return i < arr.length - 2; // there are 2 extra elements in this array always
+  }).map(e => e.id);
+  const healthLabels = Object.values($("input[class='healthLabels']:checked")).filter(function(e,i,arr){
+    return i < arr.length - 2;
+  }).map(e => e.id);
+
+    let serch = '';
+    for (let i=0; i< dietLabels.length; i++){
+      serch = serch + dietLabels[i].toString() + ' ';
+    }
+    for (let i=0; i< healthLabels.length; i++){
+      serch = serch + healthLabels[i].toString() + ' ';
+    }
+  
+  const params = {
+    key: STORE.youApiKey,
+    q: serch + query + ' ' + 'recipe',
+    part: 'snippet',
+    maxResults,
+    type: 'video'
+  };
+
+  const queryString = formatQueryParams(params)
+  const url = STORE.youSearchURL + '?' + queryString;
+
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseTubes => displayResults(responseJson, responseTubes, dietLabels, healthLabels))
     .catch(err => {
       $('#js-error-message').html("");
       $('#js-error-message').append(`Oops, something must have gone wrong: wait and try again`);
@@ -124,8 +196,8 @@ function watchForm() { //gets strings from input once 'search' is pressed
   $('form').submit(event => {
     event.preventDefault();
     const ing = $('#search-term').val();
-
-    getRests(ing);
+    const maxResults = 10;
+    getRests(ing, maxResults);
   });
 }
 
